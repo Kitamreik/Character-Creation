ef128468868ad5677dfbec91d666719f591faa88
Kit Fenrir Amreik <94805218+Kitamreik@users.noreply.github.com>
2023-08-21 16:26:41 -0400

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,17 @@
 /*
 
-Rick and Morty Bio
+Naruto Bio
 
-We are huge fans of Rick and Morty and we would like to make a biography website about the characters. Can we have the background color of the website this (#42f5b9) color and the font family to be 'Verdana, sans-serif'. There are a few span tags within each li, can we add the facts about each character in the unorderedlist. We would like to create a new image of each character. Set the src attribute to the relative path in order to show the image. Also set the width attribute to 200px. You can append the image into the card and below the unorderedlist.
+We are huge fans of Naruto and we would like to make a biography website about the characters. Can we have the background color of the website this (#42f5b9) color and the font family to be 'Verdana, sans-serif'. There are a few span tags within each li, can we add the facts about each character in the unorderedlist. We would like to create a new image of each character. Set the src attribute to the relative path in order to show the image. Also set the width attribute to 200px. You can append the image into the card and below the unorderedlist.
 
 CONTENT NEEDED FOR THIS CLASSWORK
 
-name: Rick Sanchez
-nickname: Rick C-137
-age: 70
-occupation: Scientist
-quote: Wubba lubba dub-dub!
-background-color: #42f5b9
-font-family: Verdana, Geneva, Tahoma, sans-serif
+- name: Naruto Uzumaki
+- speciality: Rasengan 
+- age: 13
+- occupation: Future Hokage
+- quote: Believe it!
+- background-color: #42f5b9
+- font-family: Verdana, Geneva, Tahoma, sans-serif
 
 */
